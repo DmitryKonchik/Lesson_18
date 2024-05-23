94906cc6763672686f9b16ff94bcc70fd742588f
Dmitry <dkonchik@gmail.com>
2024-05-23 19:53:47 +0300

    Hard Task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,5 +25,22 @@ public class Main {
 
         //Hard Task
 
+        Map<Integer, String> users = new HashMap<>();
+
+        users.put(1, "Dima");
+        users.put(2, "Den");
+        users.put(3, "Jhon");
+        users.put(5, "Bob");
+        users.put(4, "Gabriel");
+        users.put(8, "Gabriel1");
+        users.put(13, "Dilan");
+        users.put(11, "Stepan");
+
+        List<StringBuilder> list2 = users.keySet().stream()
+                .filter(u -> u == 1 || u == 2 || u == 5 || u == 8 || u == 9 || u == 13)
+                .filter(u -> users.get(u).length() % 2 != 0)
+                .map(u -> new StringBuilder(users.get(u)).reverse())
+                .collect(Collectors.toList());
+        System.out.println(list2);
     }
 }
